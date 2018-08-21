df8f6716690496ef4e0d38820a3d1336fbc5a859
Jorge Isaac Rojas Sánchez <jorgeisaac.rojas@ucrso.info>
2018-08-21 11:07:41 -0600

    Toast Buttons

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+
 
 /**
  * Generated class for the LoginPage page.
@@ -15,11 +16,53 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private toast: ToastController, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  facebookLogin() {
+    let toast = this.toast.create({
+      message: 'Facebook Login Success',
+      duration: 3000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
+  }
+
+  googleLogin() {
+    let toast = this.toast.create({
+      message: 'Google Login Success',
+      duration: 3000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
+  }
+
+  guestLogin() {
+    let toast = this.toast.create({
+      message: 'You are using the app as guest ',
+      duration: 3000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
   }
 
 }
